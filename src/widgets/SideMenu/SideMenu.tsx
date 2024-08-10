94a3dfa475244c51a26bc301a61c9a97fda0db9d
Nikolay Pushkin <adwedelf@gmail.com>
2024-08-10 19:57:15 +0700

    feat: swiper for open menu

--- src/widgets/SideMenu/SideMenu.tsx
+++ src/widgets/SideMenu/SideMenu.tsx
@@ -1,26 +1,36 @@
-import {useState} from 'react';
-import {Drawer} from 'antd';
+import { useState } from 'react';
+import { Drawer } from 'antd';
 import ChangeThemeBtn from "../../features/changeTheme/ChangeThemeBtn.tsx";
-import {MenuOutlined} from '@ant-design/icons'
-import styles from "./SideMenu.module.css"
+import { MenuOutlined } from '@ant-design/icons';
+import {useSwipeable, SwipeableHandlers, SwipeableProps} from 'react-swipeable';
+import styles from "./SideMenu.module.css";
 
-const SideMenu = () => {
-    const [open, setOpen] = useState(false);
+const SideMenu: React.FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
 
-    const showDrawer = () => {
+    const showDrawer = (): void => {
         setOpen(true);
     };
 
-    const onClose = () => {
+    const onClose = (): void => {
         setOpen(false);
     };
 
+    const handlers: SwipeableHandlers = useSwipeable({
+        onSwipedLeft: onClose,
+        onSwipedRight: showDrawer,
+        preventDefaultTouchmoveEvent: true,
+        trackTouch: true,
+    } as SwipeableProps);
+
     return (
         <>
-            <MenuOutlined className={styles["menu__icon"]} onClick={showDrawer}/>
-            <Drawer placement="left" title="Меню" onClose={onClose} open={open} extra={<ChangeThemeBtn/>}>
-                <h1>Sorry. Empty</h1>
-            </Drawer>
+            <MenuOutlined className={styles["menu__icon"]} onClick={showDrawer} />
+            <div {...handlers}>
+                <Drawer placement="left" title="Меню" onClose={onClose} open={open} extra={<ChangeThemeBtn />}>
+                    <h1>Sorry. Empty</h1>
+                </Drawer>
+            </div>
         </>
     );
 };
